feef49369e4717feabd0691150f5d36c22e7700d
57066698 <57066698@qq.com>
2020-08-23 22:57:37 +0800

    0.1.0

--- rotateExample/rotateScene.py
+++ rotateExample/rotateScene.py
@@ -1,11 +1,12 @@
-from simple3D import Scene, DisplayObject, Mesh, Camera, MouseMove
+from simple3D import Scene, DisplayObject, Mesh
+from simple3D.components.moseMoveDisplayObject import MouseMoveDisplayObject
 from simple3D.mats.lineMeterial import LineMeterial
 import numpy as np
 
 vertices = [0.0, 0.0, 0.0,
             1, 0, 0.0,
             0, 0, 0,
-              0, 1, 0,
+            0, 1, 0,
             0, 0, 0,
             0, 0, 1]
 
@@ -18,20 +19,27 @@ vertices_color = [1, 0, 0,
 
 indices = [0, 1, 2, 3, 4, 5]
 
+
 class RotateScene:
     def __init__(self):
         self.axis1 = self.get_axis()
         self.axis2 = self.get_axis()
 
-        self.axis2.transfrom.pos += np.array([1, 0, 0])
-        self.axis1.transfrom.pos += np.array([-1, 0, 0])
+        self.axis2.transform.pos += np.array([1, 0, 0])
+        self.axis1.transform.pos += np.array([-1, 0, 0])
 
         scene = Scene()
         scene.add(self.axis1, self.axis2)
-        mouseMove = MouseMove(scene) # 集中到scene
-        camera = scene.default_camera
-        mouseMove.set_camera(camera) #
+        mouseMove = MouseMoveDisplayObject(scene)
+        mouseMove.add(self.axis1)  #
         scene.add(mouseMove)
+
+        def func():
+            euler = self.axis1.transform.euler
+            self.axis2.transform.euler = euler
+
+        scene.add(func)
+
         scene.render()
 
     def get_axis(self):
@@ -39,6 +47,7 @@ class RotateScene:
         material = LineMeterial()
         axis = DisplayObject(mesh, material)
         return axis
+
 
 if __name__ == "__main__":
     rotateScene = RotateScene()
