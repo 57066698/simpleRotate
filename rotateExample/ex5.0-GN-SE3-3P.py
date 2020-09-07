fea81c634ba6646c7109293beb9e6c5fb42701ab
57066698 <57066698@qq.com>
2020-09-07 08:58:52 +0800

    0.5.0

--- rotateExample/ex5.0-GN-SE3-3P.py
+++ rotateExample/ex5.0-GN-SE3-3P.py
@@ -11,29 +11,30 @@ def box_plus(a, b):
 
 def cal():
     rotation = rotateScene.axis2.transform.rotation
+    trans = rotateScene.axis2.transform.pos
     R2 = SO3.from_matrix(rotation, normalize=True)
     P = np.vstack(([1., 0., 0.], [0., 1., 0.], [0., 0., 1.]))
-    R2P = np.dot(R2.mat, P.transpose()).transpose()
-    P_ = np.dot(rotateScene.axis1.transform.rotation, P.transpose()).transpose()
+    R2P = np.dot(R2.mat, P.transpose()).transpose() + trans
+    P_ = np.dot(rotateScene.axis1.transform.rotation, P.transpose()).transpose() + rotateScene.axis1.transform.pos
     fR = (R2P - P_).reshape(-1)
 
     J1 = -SO3.wedge(R2P[0, :])
     J2 = -SO3.wedge(R2P[1, :])
     J3 = -SO3.wedge(R2P[2, :])
 
-    J4 = np.array([-1.0, 0, 0])
-    J5 = np.array([.0, -1.0, 0])
-    J6 = np.array([.0, .0, -1])
-
-    J = np.concatenate((J1, J2, J3, J4, J5, J6), axis=0)
+    J = np.zeros((9, 6))
+    J[:, :3] = np.concatenate((J1, J2, J3), axis=0)
+    J[:, 3:6] = np.concatenate((np.identity(3), np.identity(3), np.identity(3)), axis=0)
     Jt = np.transpose(J)
-    JtJ = np.dot(Jt, J2) + np.identity(3) * 1e-8
+    JtJ = np.dot(Jt, J) + np.identity(6) * 1e-8
     res = - np.dot(Jt, fR)
     r_ = np.dot(np.linalg.inv(JtJ), res)
 
     r_rotate = r_[:3]
     r_translate = r_[3:6]
     R2 = box_plus(R2, r_rotate)
+
+    print(r_)
 
     rotateScene.axis2.transform.rotation = R2.mat
     rotateScene.axis2.transform.pos += r_translate
